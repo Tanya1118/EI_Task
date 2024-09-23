cb322885076e4a5f860b3ca25585ca9a88770c4c
Tanya1118 <128591833+Tanya1118@users.noreply.github.com>
2024-09-24 00:52:22 +0530

    Add files via upload

---
+++ Exercise 1/Structural/Decorator/DecoratorPatternExample.java
@@ -0,0 +1,84 @@
+// Component
+interface Coffee {
+    String getDescription();
+    double cost();
+}
+
+// Concrete Component
+class SimpleCoffee implements Coffee {
+    @Override
+    public String getDescription() {
+        return "Simple Coffee";
+    }
+
+    @Override
+    public double cost() {
+        return 5.0;
+    }
+}
+
+// Decorator
+abstract class CoffeeDecorator implements Coffee {
+    protected Coffee decoratedCoffee;
+    
+    public CoffeeDecorator(Coffee coffee) {
+        this.decoratedCoffee = coffee;
+    }
+    
+    @Override
+    public String getDescription() {
+        return decoratedCoffee.getDescription();
+    }
+    
+    @Override
+    public double cost() {
+        return decoratedCoffee.cost();
+    }
+}
+
+// Concrete Decorator: Milk
+class MilkDecorator extends CoffeeDecorator {
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", Milk";
+    }
+
+    @Override
+    public double cost() {
+        return super.cost() + 1.5;
+    }
+}
+
+// Concrete Decorator: Sugar
+class SugarDecorator extends CoffeeDecorator {
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", Sugar";
+    }
+
+    @Override
+    public double cost() {
+        return super.cost() + 0.5;
+    }
+}
+
+public class DecoratorPatternExample {
+    public static void main(String[] args) {
+        Coffee coffee = new SimpleCoffee();
+        System.out.println(coffee.getDescription() + " $" + coffee.cost());
+        
+        coffee = new MilkDecorator(coffee);
+        System.out.println(coffee.getDescription() + " $" + coffee.cost());
+        
+        coffee = new SugarDecorator(coffee);
+        System.out.println(coffee.getDescription() + " $" + coffee.cost());
+    }
+}
